c26f87986ceabe165fc41512f467c73b2a1a0a3f
Ahi-Tech86 <ahi-tech86@proton.me>
2025-06-30 15:12:58 +0300

    assets(objects/level): add level map files

---
+++ src/main/resources/levels/full_basic_objects.tsx
@@ -0,0 +1,168 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="full_basic_objects" tilewidth="16" tileheight="16" tilecount="40" columns="10">
+ <image source="objects/full_basic_objects.png" width="160" height="64"/>
+ <tile id="3">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fenceCorner"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fence"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fenceCornerRight"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="name" value="bush"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="chest"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="box"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fenceSide"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fenceSide2"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="cornerFence"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="fence"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="cornerFence"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="collision" type="bool" value="false"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="tent"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="name" value="healthPack"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="name" value="ammo"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="name" value="shotgun"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="name" value="fenceBeamRight"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="name" value="fenceBeamLeft"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="name" value="bushType1"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="name" value="bushType2"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="name" value="bushType3"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="name" value="bushType4"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ src/main/resources/levels/treesSpriteSheet.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="treesSpriteSheet" tilewidth="16" tileheight="32" tilecount="4" columns="1">
+ <image source="objects/treesSpriteSheet.png" width="16" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="treeType1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="treeType2"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="treeType3"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="collision" type="bool" value="true"/>
+   <property name="name" value="treeType4"/>
+  </properties>
+ </tile>
+</tileset>
